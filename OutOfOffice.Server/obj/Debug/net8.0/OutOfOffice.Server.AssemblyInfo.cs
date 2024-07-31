c91ca77de3eccff884d85085a21e5241f36dc6d7
MichalOSProject <m.osicki.01@gmail.com>
2024-07-31 16:36:41 +0200

    Test Update #2

--- OutOfOffice.Server/obj/Debug/net8.0/OutOfOffice.Server.AssemblyInfo.cs
+++ OutOfOffice.Server/obj/Debug/net8.0/OutOfOffice.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OutOfOffice.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ffb7b4897d89dc3cee02ab8dc3da9d5eeb6c622")]
 [assembly: System.Reflection.AssemblyProductAttribute("OutOfOffice.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OutOfOffice.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- outofoffice.client/src/components/Pages/home.jsx
+++ outofoffice.client/src/components/Pages/home.jsx
@@ -1,5 +1,5 @@
 const Home = () => {
-    return <h1>Home</h1>;
+    return <h1>Out of Office - Home Site</h1>;
 };
 
 export default Home;
